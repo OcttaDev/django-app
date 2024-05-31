552c9262a0585510abcfc764a4e78d2ee6138368
Otavio <aotavio061@gmail.com>
2024-05-31 05:01:52 -0300

    feat: criando templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def blog(request):
+    print("[0 - REQUESTS]")
+    return render(
+        request,
+        template_name='blog/index.html'
+    )
--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+
+    print("[0 - REQUESTS]")
+    return render(
+        request,
+        template_name="home/index.html"
+    )
